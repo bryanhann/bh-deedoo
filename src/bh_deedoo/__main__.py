70c5d7855c3d9d3e0ad91673acc0d078d008e18a
Bryan Hann <nobody@nowhere>
2024-12-17 12:29:53 -0330

    Add pytests.

--- src/bh_deedoo/__main__.py
+++ src/bh_deedoo/__main__.py
@@ -1,9 +1,6 @@
 import bh_deedoo.cmds.deedoo as deedoo
-import bh_deedoo.cmds.deedoo2 as deedoo2
 
 import typer
-app = typer.Typer()
-app.add_typer(deedoo.app, name="deedoo")
-app.add_typer(deedoo2.app, name="deedoo2")
+
 def main():
-    app()
+    deedoo.app()
--- src/bh_deedoo/cmds/deedoo/__init__.py
+++ src/bh_deedoo/cmds/deedoo/__init__.py
@@ -1,36 +1,30 @@
-import os
+#!/usr/bin/env python3
 
 import typer
+import schedule
 
-import bh_deedoo.cmds.deedoo.stoppers as STOPPERS
-import bh_deedoo.cmds.deedoo.server as SERVER
+from .lib.debug import DEBUG
+from .lib.stop import STOPS, stoppers
+from .main import run
 
 app = typer.Typer()
 
 @app.command()
-def kill():
-    """Kill a running server"""
-    STOPPERS.kill()
+def answer(code : str ):
+    STOPS.add(code)
 
 @app.command()
-def serve(debug: bool=False):
-    """Run the server [& to run in backgound]"""
-    SERVER.run_server( debug = debug )
+def list():
+    """Show all stoppers"""
+    items = sorted(stoppers().items())
+    while items:
+        for hhmm,stop in items[:4]:
+            print(hhmm,stop, end='\t')
+        items = items[4:]
+        print()
 
 @app.command()
-def answer(candidate: str):
-    """Answer a deedoo"""
-    STOPPERS.create(candidate)
-
-@app.command()
-def stoppers(time: bool=False):
-    """Show all stoppers [--time to add time]"""
-    items = sorted(STOPPERS.stoppers().items())
-    for hhmm,stop in items:
-        if time:
-            print(hhmm,stop)
-        else:
-            print(stop)
-
-
+def serve(debug : bool = False):
+    DEBUG.set_state(debug)
+    run()
 
